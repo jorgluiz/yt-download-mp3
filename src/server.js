0eedce3fa993d6920f4fac452c547d69ea27f91d
jorgluiz <jorgeluizti@hotmail.com>
2024-11-27 15:11:25 -0300

    remove requestOptions

--- src/server.js
+++ src/server.js
@@ -114,10 +114,8 @@ const processVideo = async (req, res) => {
       format: audio,
       filter: "audioonly",
       quality: "highestaudio",
-      requestOptions: {
-        headers: {
-          "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
-        },
+      headers: {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
       },
     })
 
